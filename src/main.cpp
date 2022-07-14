f2fb7965b66aea3797256bd173c9f1920df43ea8
aoiyu <aoiyu.044f@gmail.com>
2022-07-14 11:14:06 +0900

    Add cuda example implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/cudaarithm.hpp"
+#include "opencv2/cudaimgproc.hpp"
 #include <list>
 
 const static int N = 13;
@@ -53,6 +56,20 @@ const static double An[N] = {
 //        3.382710778154774e-17
 };
 
+std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    using namespace std;
+    return "nvarguscamerasrc ! "
+           "video/x-raw(memory:NVMM), "
+           "width=" + to_string(capture_width) + ", height=" + to_string(capture_height) + ", " // "format=(string)NV12, "
+           "framerate=(fraction)" + to_string(framerate) + "/1 ! "
+           "nvvidconv flip-method=" + to_string(flip_method) + " ! "
+           "video/x-raw, width=" + to_string(display_width) + ", height=" + to_string(display_height) + ", format=(string)BGRx ! "
+           "videoconvert ! "
+           "video/x-raw, format=(string)GRAY8 ! "
+//           "nv3dsink";
+           "appsink";
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
@@ -61,11 +78,10 @@ int main() {
         using namespace cv;
         Mat capBuffer;
         VideoCapture cap;
-        cap.set(CAP_PROP_FRAME_WIDTH, 1280);
-        cap.set(CAP_PROP_FRAME_HEIGHT, 720);
-        cap.set(CAP_PROP_FPS, 60);
 
-        cap.open(0, cv::CAP_ANY);
+        string pipeline = gstreamer_pipeline(1280, 720, 1280, 720, 60, 0);
+
+        cap.open(pipeline, CAP_GSTREAMER);
 
         if (!cap.isOpened()) {
             cerr << "failed to open camera id:0" << endl;
@@ -78,43 +94,59 @@ int main() {
             cout << "loading first frame" << endl;
         }
 
+        cout << "row:col " << output.rows << ":" << output.cols << endl;
+
+        auto lastTime = chrono::high_resolution_clock::now();
+        int frameCount = 0;
+        cuda::GpuMat img_gpu_src, img_gpu_dst;
         for (;;) {
             auto result = cap.read(capBuffer);
 
             if (result) {
-                auto begin = chrono::high_resolution_clock::now();
 
-                cv::cvtColor(capBuffer, output, COLOR_BGR2GRAY);
-                //auto frame = capBuffer.clone();
-                buffer.push_front(output);
+////                if (buffer.size() >= N) {
+////                    output.forEach<uchar>([&](uchar &p, const int* position) -> void {
+////                        p = 0; // clear previous output
+////                        int i = 0;
+////                        for (const auto &item: buffer) {
+////                            p += (double)item.at<int>(position) * An[i];
+////                            ++i;
+////                            if (i > N) break;
+////                        }
+////                    });
+////                    buffer.pop_back();
+////                } else {
+////                    cout << "buffering... " << buffer.size() << endl;
+////                }
 
-                if (buffer.size() >= N) {
-                    output.forEach<uchar>([&](uchar &p, const int* position) -> void {
-                        // p = 0; // clear previous output
-                        int i = 0;
-                        for (const auto &item: buffer) {
-                            p += (double)item.at<int>(position) * An[i];
-                            ++i;
-                            if (i > N) break;
-                        }
-                    });
-                    buffer.pop_back();
-                } else {
-                    cout << "buffering... " << buffer.size() << endl;
+                // Cuda test
+                try {
+                    img_gpu_src.upload(capBuffer);
+                    cv::cuda::threshold(img_gpu_src, img_gpu_dst, 128, 255, THRESH_BINARY);
+                    // cv::cuda::bilateralFilter(img_gpu_src, img_gpu_dst, 3, 1 , 1);
+                    img_gpu_dst.download(output);
+                } catch (const cv::Exception& ex) {
+                    cout << "Error: " << ex.what() << endl;
                 }
 
+                // Calculate time
                 auto end = chrono::high_resolution_clock::now();
-                // cout << "time elapsed " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << "[ns]" << endl;
+                cout << "fps:" << 1.0 / (chrono::duration_cast<chrono::nanoseconds>(end - lastTime).count() / 1000000000.0) << endl;
+                lastTime = end;
+
+               if (frameCount % 10 == 0 && !output.empty()) {
+                   frameCount = 0;
+                   imshow("Live", capBuffer);
+                   imshow("Output", output);
+                   if (waitKey(1) >= 0) break;
+               }
+
+               ++frameCount;
             } else {
                 cout << "capBuffer skipped " << endl;
             }
-
-            if (!output.empty()) {
-                // imshow("Live", capBuffer);
-                imshow("Output", output);
-                if (waitKey(1) >= 0) break;
-            }
         }
+        cap.release();
     }
 
     return 0;
