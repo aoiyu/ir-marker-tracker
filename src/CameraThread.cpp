416bf74534fbf0ee76f0da574ce4228d5a1430e6
aoiyu <aoiyu.044f@gmail.com>
2022-07-15 14:18:29 +0900

    Add thread debug implementation in CameraThread::start()

--- src/CameraThread.cpp
+++ src/CameraThread.cpp
@@ -2,6 +2,8 @@
 // Created by enterc on 2022/07/14.
 //
 
+#include <thread>
+#include <future>
 #include <list>
 #include <iostream>
 #include <opencv2/highgui.hpp>
@@ -16,9 +18,6 @@ namespace irmt {
                "framerate=(fraction)" + to_string(framerate) + "/1 ! " \
                "nvvidconv flip-method=" + to_string(flipMethod) + " ! " \
                "video/x-raw, width=" + to_string(displayWidth) + ", height=" + to_string(displayHeight) + ", format=(string)GRAY8 ! " \
-               // "videoconvert ! " \
-               // "video/x-raw, format=(string)GRAY8 ! " \
-               //           "nv3dsink";
                "appsink";
     }
 
@@ -29,7 +28,8 @@ namespace irmt {
         using namespace std;
         using namespace cv;
 
-        { // Open camera
+        auto setupExit = promise<void>();
+        auto setupCamera = [&](future<void> exitSignal){ // Open camera
             string pipeline = irmt::gstreamerPipeline(1280, 720, 1280, 720, 60, 0);
             this->camera = cv::VideoCapture(pipeline, cv::CAP_GSTREAMER);
 
@@ -39,35 +39,54 @@ namespace irmt {
                 cerr << "failed to open camera id:0" << endl;
                 return;
             }
-        }
 
-        { // Capture
-            auto buffer = std::list<Mat>();
-            Mat captured, output;
-            while(!camera.read(output)) {
-                cout << "loading first frame" << endl;
+            cout << "trying to capture first frame" << endl;
+            while(exitSignal.wait_for(chrono::microseconds(1)) == future_status::timeout) {
+                static Mat testFrame;
+                bool result = camera.read(testFrame);
+                if (result) {
+                    cout << "testFrame captured. row:col=" << testFrame.rows << ":" << testFrame.cols << endl;
+                    break;
+                }
             }
+            cout << "exit capture first frame" << endl;
+        };
 
-            cout << "row:col " << output.rows << ":" << output.cols << endl;
+        auto captureExit = promise<void>();
+        auto captureFrame = [&camera = this->camera](future<void> exitSignal){ // Capture
+            auto buffer = std::list<Mat>();
+            Mat captured;
 
             auto lastTime = chrono::high_resolution_clock::now();
             int frameCount = 0;
-            // cuda::GpuMat img_gpu_src, img_gpu_dst;
-            for (;;) {
+            // cuda::GpuMat img_gpu_src, img_gpu_dst;n
+            while (exitSignal.wait_for(chrono::microseconds(1)) == future_status::timeout) {
                 auto result = camera.read(captured);
                 if (result) {
-                    if (frameCount % 10 == 0 && !output.empty()) {
+                    if (frameCount % 1 == 0 && !captured.empty()) {
                         frameCount = 0;
                         imshow("Live", captured);
-                        // imshow("Output", output);
-                        //if (waitKey(1) >= 0) break;
+                        if (waitKey(1) >= 0) break;
                     }
-
                     ++frameCount;
                 } else {
                     cout << "capture skipped " << endl;
                 }
             }
+            cout << "exit capture frame" << endl;
+        };
+
+        setupCamera(setupExit.get_future());
+        thread th1(captureFrame, captureExit.get_future());
+
+        auto delay = chrono::milliseconds(500);
+        for (int i = 0; i < 10; ++i) {
+            cout << "current " << i << endl;
+            this_thread::sleep_for(delay);
         }
+        captureExit.set_value();
+
+        th1.join(); // wait to join th1
+        camera.release();
     }
 } // irmt
